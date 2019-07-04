962024171258e1cea6938467df1468746faa1356
André Robert <fxxdev@outlook.com>
2019-07-04 08:18:02 -0300

    Upload

--- freeway.cpp
+++ freeway.cpp
@@ -7,6 +7,7 @@
 #include <GL/freeglut.h>
 #include <GL/glu.h>
 #include<math.h>
+#include <sstream>
 
 #define windowH 700 
 #define windowW 1000 
@@ -49,19 +50,35 @@ int r5 =0,g5=50,b5=50;
 
 //Player
 float p1X = -300, p1Y = -285, p1Comp = 20, p1Alt = 20;
-float p2X = -300, p2Y = -285, p2Comp = 20, p2Alt = 20;
+float p2X = 300, p2Y = -285, p2Comp = 20, p2Alt = 20;
+int scorep1=0;
+int scorep2=0;
 
 void keyboard(unsigned char key, int x, int y) {
 	//if (p1Y > 250) Sleep(2000), exit(0);
-	if (key == 'w') if (p1Y < 250) p1Y+=25;
+	if (key == 'w') if (p1Y < 275) p1Y+=25;
 	if (key == 's') if (p1Y > -250) p1Y -= 25;
 
-	if (key == 'o') if (p2Y < 250) p2Y+=25;
+	if (key == 'o') if (p2Y < 275) p2Y+=25;
 	if (key == 'l') if (p2Y > -250) p2Y -= 25;
+
+	if(p1Y > 250) p1Y=-285; scorep1++;
+	if(p2Y > 250) p2Y=-285; scorep2++;
  	
+}
+std::string int2str(int x) {
+	// converter inteiro para string
+	std::stringstream ss;
+	ss << x;
+	return ss.str();
 }
 
 
+void drawText(float x, float y, std::string text) {
+	glColor3ub(255,0,0);
+	glRasterPos2f(x, y);
+	glutBitmapString(GLUT_BITMAP_8_BY_13, (const unsigned char*)text.c_str());
+}
 
 //Colision
 bool Colision(float Ax, float Ay, float Bx, float By, float Bcompr, float Baltu) {
@@ -99,15 +116,25 @@ void update(int value) {
 	int y;
 	keyboard(key,x,y);
 	glutTimerFunc(interval, update, 0);
+
 	glutPostRedisplay();
 }
 void anima(int valor) {
 
-	if (Colision(tx0, ty0, p1X, p1Y, p1Comp, p1Alt) == true) p1Y = -285;
+	if (Colision(tx0, ty0, p1X, p1Y, p1Comp, p1Alt) == true) p1Y = -285 ;
 	if (Colision(tx1, ty1, p1X, p1Y, p1Comp, p1Alt) == true) p1Y = -285;
 	if (Colision(tx2, ty2, p1X, p1Y, p1Comp, p1Alt) == true) p1Y = -285;
 	if (Colision(tx3, ty3, p1X, p1Y, p1Comp, p1Alt) == true) p1Y = -285;
 	if (Colision(tx4, ty4, p1X, p1Y, p1Comp, p1Alt) == true) p1Y = -285;
+
+	if (Colision(tx0, ty0, p2X, p2Y, p2Comp, p2Alt) == true) p2Y = -285;
+	if (Colision(tx1, ty1, p2X, p2Y, p2Comp, p2Alt) == true) p2Y = -285;
+	if (Colision(tx2, ty2, p2X, p2Y, p2Comp, p2Alt) == true) p2Y = -285;
+	if (Colision(tx3, ty3, p2X, p2Y, p2Comp, p2Alt) == true) p2Y = -285;
+	if (Colision(tx4, ty4, p2X, p2Y, p2Comp, p2Alt) == true) p2Y = -285;
+
+
+	
 
 
 	if (tx0 < -490)	tx0 = 500.0f;
@@ -124,8 +151,9 @@ void anima(int valor) {
 	tx4 += spd4;
 	
 	glutPostRedisplay();
+	drawText(windowW / 2 , windowH , +"P1:"+ int2str(scorep1) + "  vs  " + "P2:" +int2str(scorep2));
 	glutTimerFunc(15, anima, 2);
-
+	drawText(windowW / 2 , windowH , +"P1:"+ int2str(scorep1) + "  vs  " + "P2:" +int2str(scorep2));	
 }
 
 
@@ -195,14 +223,7 @@ void player1() {
 
 	glPushMatrix();
 	
-	/*glBegin(GL_QUADS);
-	glColor3ub(245, 233, 66);
-	glVertex2f(p1X, p1Y + p1Alt);
-	glVertex2f(p1X + p1Comp, p1Y + p1Alt);
-	glVertex2f(p1X + p1Comp, p1Y);
-	glVertex2f(p1X, p1Y);
-	glEnd();*/
-
+	
 	glBegin(GL_QUADS);
 	glColor3ub(245, 233, 66);
 	glVertex2f(p1X + 15, p1Y + p1Alt + 10);
@@ -211,13 +232,21 @@ void player1() {
 	glVertex2f(p1X + 15, p1Y + 10);
 	glEnd();
 
-	/*glBegin(GL_QUADS);
+	glBegin(GL_QUADS);
 	glColor3ub(255, 0, 0);
 	glVertex2f(p1X + 15, p1Y + p1Alt + 10);
 	glVertex2f(p1X - p1Comp + 30, p1Y + p1Alt + 5);
 	glVertex2f(p1X + 15, p1Y + 10);
 	glVertex2f(p1X + 15, p1Y + 10);
-	glEnd();*/
+	glEnd();
+
+	glBegin(GL_QUADS);
+	glColor3ub(255, 255, 255);
+	glVertex2f(p1X + 20, p1Y + p1Alt + 10);
+	glVertex2f(p1X - p1Comp + 30, p1Y + p1Alt + 10);
+	glVertex2f(p1X + 20, p1Y + 10);
+	glVertex2f(p1X + 15, p1Y + 10);
+	glEnd();	
 
 	glBegin(GL_QUADS);
 	glColor3ub(255, 255, 255);
@@ -231,20 +260,79 @@ void player1() {
 	glColor3ub(82, 70, 52);
 	glVertex2f(p1X +14, p1Y);
 	glVertex2f(p1X + p1Comp -3, p1Y);
-	glVertex2f(p1X + p1Comp -3, p1Y - 8);
+	glVertex2f(p1X + p1Comp -3, p1Y - 12);
 	glVertex2f(p1X + 14, p1Y - 8);
 	glEnd();
 
 	glBegin(GL_QUADS);
 	glColor3ub(82, 70, 52);
-	glVertex2f(p1X + 4, p1Y);
+	glVertex2f(p1X + 4, p1Y+2);
 	glVertex2f(p1X + p1Comp - 13, p1Y);
-	glVertex2f(p1X + p1Comp - 13, p1Y - 8);
-	glVertex2f(p1X + 4, p1Y - 8);
+	glVertex2f(p1X + p1Comp - 13, p1Y - 12);
+	glVertex2f(p1X + 4, p1Y - 12);
 	glEnd();
 	glPopMatrix();
 
 }
+
+void player2() {
+
+	glPushMatrix();
+	
+	
+	glBegin(GL_QUADS);
+	glColor3ub(245, 233, 66);
+	glVertex2f(p2X + 15, p2Y + p2Alt + 10);
+	glVertex2f(p2X + p2Comp + 5, p2Y + p2Alt + 5);
+	glVertex2f(p2X + 15, p2Y + 10);
+	glVertex2f(p2X + 15, p2Y + 10);
+	glEnd();
+
+	glBegin(GL_QUADS);
+	glColor3ub(255, 0, 0);
+	glVertex2f(p2X + 15, p2Y + p2Alt + 10);
+	glVertex2f(p2X - p2Comp + 30, p2Y + p2Alt + 5);
+	glVertex2f(p2X + 15, p2Y + 10);
+	glVertex2f(p2X + 15, p2Y + 10);
+	glEnd();
+
+	glBegin(GL_QUADS);
+	glColor3ub(0, 0, 0);
+	glVertex2f(p2X + 20, p2Y + p2Alt + 10);
+	glVertex2f(p2X - p1Comp + 30, p2Y + p2Alt + 10);
+	glVertex2f(p2X + 20, p2Y + 10);
+	glVertex2f(p2X + 15, p2Y + 10);
+	glEnd();	
+
+	glBegin(GL_QUADS);
+	glColor3ub(0, 0, 0);
+	glVertex2f(p2X - 10, p2Y + p2Alt);
+	glVertex2f(p2X + p2Comp, p2Y + p2Alt);
+	glVertex2f(p2X + p2Comp, p2Y + 4);
+	glVertex2f(p2X, p2Y + 8);
+	glEnd();
+
+	glBegin(GL_QUADS);
+	glColor3ub(82, 70, 52);
+	glVertex2f(p2X +14, p2Y);
+	glVertex2f(p2X + p2Comp -3, p2Y);
+	glVertex2f(p2X + p2Comp -3, p2Y - 12);
+	glVertex2f(p2X + 14, p2Y - 8);
+	glEnd();
+
+	glBegin(GL_QUADS);
+	glColor3ub(82, 70, 52);
+	glVertex2f(p2X + 4, p2Y+2);
+	glVertex2f(p2X + p2Comp - 13, p2Y);
+	glVertex2f(p2X + p2Comp - 13, p2Y - 12);
+	glVertex2f(p2X + 4, p2Y - 12);
+	glEnd();
+	glPopMatrix();
+
+}
+
+
+
 float Car(float tx, float ty, int r, int g, int b) {
 
 	glPushMatrix();
@@ -302,6 +390,7 @@ void display()
 	
 	environment();
 	player1();
+	player2();
 
 	//cars
 	Car(tx0, ty0,r1,g1,b1);
@@ -310,7 +399,7 @@ void display()
 	Car(tx3, ty3,r3,g4,b4);
 	Car(tx4, ty4,r4,g5,b5);
 	
-
+	
 	glFlush();
 
 }
